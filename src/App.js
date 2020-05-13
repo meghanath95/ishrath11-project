84b0d112cf56ef60d87de68623eaf0ea783ddf0f
ishrath11 <ishuishrath11@gmail.com>
2020-05-13 01:18:10 -0500

    Assign-5
    Assignment-5

--- src/App.js
+++ src/App.js
@@ -1,69 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-//import Nav from './Nav';
-//import Italian from './Italian';
-//import Indian from './Indian'; 
-//import Mexican from './Mexican';
+import Nav from './Nav';
+import Italian from './Italian';
+import Indian from './Indian'; 
+import Mexican from './Mexican';
 import { BrowserRouter as Router, Switch, Route}  from 'react-router-dom';
 
 
-class DynamicImport extends Component {
-  state = {
-    component: null
-  }
-  componentDidMount () {
-    this.props.load()
-      .then((component) => {
-        this.setState(() => ({
-          component: component.default ? component.default : component
-        }))
-      })
-  }
-  render() {
-    return this.props.children(this.state.component)
-  }
-}
-
-
-//Nav bar
-const Nav = (props) => (
-  <DynamicImport load={() => import('./Nav')}>
-    {(Component) => Component === null
-      ? <p>Loading</p>
-      : <Component {...props} />}
-  </DynamicImport>
-)
-
-
-
-//Italian
-const Italian = (props) => (
-  <DynamicImport load={() => import('./Italian')}>
-    {(Component) => Component === null
-      ? <p>Loading</p>
-      : <Component {...props} />}
-  </DynamicImport>
-)
-
-
-//Indian
-const Indian = (props) => (
-  <DynamicImport load={() => import('./Indian')}>
-    {(Component) => Component === null
-      ? <p>Loading</p>
-      : <Component {...props} />}
-  </DynamicImport>
-)
-
-
-//Mexican 
-const Mexican = (props) => (
-  <DynamicImport load={() => import('./Mexican')}>
-    {(Component) => Component === null
-      ? <p>Loading</p>
-      : <Component {...props} />}
-  </DynamicImport>
-)
 
 function App() {
   return (
